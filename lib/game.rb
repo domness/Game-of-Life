bcb2cac17e4750b6d89865a107bbe35304749597
Dominic Wroblewski <domness@gmail.com>
2011-05-23 22:58:50 +0100

    Setting up cell with a couple of basic rules.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,16 @@
 class Cell
   attr_reader :state
+  attr_accessor :neighbours_count
+
   def initialize
-    @state = :dead
+    @state = :alive
+  end
+
+  def next_state
+    if @neighbours_count == 2
+      @state = :alive
+    else
+      @staet = :dead
+    end
   end
 end
