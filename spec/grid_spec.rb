98f6ae4f39a4124738f4ab4ed89254948d13e28b
Dominic Wroblewski <domness@gmail.com>
2011-05-25 22:05:01 +0100

    Code cleanup

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -26,18 +26,22 @@ describe Grid do
   it "should have 3 neighbours for corner cells" do
     cell = @grid.cell_at(0, 0)
     cell.neighbours_count.should == 3
+
     cell = @grid.cell_at(0, COLUMNS - 1)
     cell.neighbours_count.should == 3
+
     cell = @grid.cell_at(ROWS - 1, COLUMNS - 1)
     cell.neighbours_count.should == 3
+
     cell = @grid.cell_at(ROWS - 1, 0)
     cell.neighbours_count.should == 3
   end
 
-  it "should have 5 neighbours in the first and last row" do
+  it "should have 5 neighbours in the first and last row except the corners" do
     for column in 1...COLUMNS-1
       cell = @grid.cell_at(0, column)
       cell.neighbours_count.should == 5
+
       cell = @grid.cell_at(ROWS - 1, column)
       cell.neighbours_count.should == 5
     end
