ffa4c6e15484e054133c54f1ba826325288ba088
Dominic Wroblewski <domness@gmail.com>
2011-05-25 22:01:30 +0100

    Testing cells in the corners of the grid.

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,9 +1,12 @@
 require 'grid'
 require 'cell'
 
+ROWS = 5
+COLUMNS = 10
+
 describe Grid do
   before(:each) do
-    @grid = Grid.new(5,10)
+    @grid = Grid.new(ROWS, COLUMNS)
   end
 
   it "should contain a grid of cells" do
@@ -18,6 +21,17 @@ describe Grid do
 
   it "should have a column count" do
     @grid.column_count.should == 10
+  end
+
+  it "corner cells should have neighbours" do
+    cell = @grid.cell_at(0, 0)
+    cell.neighbours_count.should == 3
+    cell = @grid.cell_at(0, COLUMNS - 1)
+    cell.neighbours_count.should == 3
+    cell = @grid.cell_at(ROWS - 1, COLUMNS - 1)
+    cell.neighbours_count.should == 3
+    cell = @grid.cell_at(ROWS - 1, 0)
+    cell.neighbours_count.should == 3
   end
 
   after(:each) do
